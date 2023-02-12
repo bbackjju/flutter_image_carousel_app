b818855f34c28fdaf402a12c2ddbb4b00fc29d46
JuHan <iiiamst16@gmail.com>
2023-02-12 22:31:56 +0900

    second commit

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,18 +1,62 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+      (timer) {
+        int? nextPage = pageController.page?.toInt();
+
+        if(nextPage == null){
+          return;
+        }
+
+        if(nextPage == 5) {
+          nextPage = 0;
+        }else {
+          nextPage++;
+        }
+        pageController.animateToPage(
+          nextPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
+      }
+    );
+  }
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    final List<Image> images = [];
+
+    for(int i=1; i<6; i++){
+      images.add(Image.asset('assets/img/image_$i.jpeg'));
+    }
+
+    images.add(Image.network('https://cdn.newspenguin.com/news/photo/201912/877_1419_234.jpg'));
+
     return Scaffold(
       body: PageView(
-        children: [1, 2, 3, 4, 5].map(
-          (number) => Image.asset('assets/img/image_$number.jpeg',
-          fit: BoxFit.cover,)
-          ).toList(),
+        controller: pageController,
+        children: images,
       ),
     );
   }
